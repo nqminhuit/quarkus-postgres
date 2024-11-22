7a9b17331fc6ff26f480503eb35a0d49c4e933ba
minh <nqminhuit@gmail.com>
2024-11-22 20:02:08 +0700

    change id datatype to uuid

--- src/main/java/org/acme/entity/SiteMap.java
+++ src/main/java/org/acme/entity/SiteMap.java
@@ -1,11 +1,20 @@
 package org.acme.entity;
 
 import java.util.List;
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import java.util.UUID;
+import org.acme.dto.SiteMapDto;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.transaction.Transactional;
 
 @Entity(name = "sitemap")
-public class SiteMap extends PanacheEntity {
+public class SiteMap extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue
+    public UUID id;
 
     public Integer year;
     public Integer month;
--- src/main/resources/db/migration/V1.0.1__insert_dummy_data.sql
+++ src/main/resources/db/migration/V1.0.1__insert_dummy_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO sitemap (id, year, month, url) values
-(1, 2024, 11, 'www.abc.com/2024/11'),
-(2, 2024, 10, 'www.abc.com/2024/10'),
-(3, 2024, 9, 'www.abc.com/2024/09'),
-(4, 2024, 8, 'www.abc.com/2024/08');
+(gen_random_uuid(), 2024, 11, 'www.abc.com/2024/11'),
+(gen_random_uuid(), 2024, 10, 'www.abc.com/2024/10'),
+(gen_random_uuid(), 2024, 09, 'www.abc.com/2024/09'),
+(gen_random_uuid(), 2024, 08, 'www.abc.com/2024/08');
